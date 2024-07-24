429db288fea32595d5a6455120b1fdd5ce87f996
= <=>
2024-07-24 21:42:00 +0700

    even better api extraction and normal mode

--- scripts/UI/tictactoe_display.gd
+++ scripts/UI/tictactoe_display.gd
@@ -1,15 +1,21 @@
-extends Control
+class_name TicTacToeDisplay extends Control
 
 @export var game_manager: GameManager;
-@export var grid_buttons: Array[GridButton]
+@export var grid_btns_parent : GridContainer;
 @export var line: Line2D;
-
 @export var exit_btn: Button;
+
+var _grid_buttons: Array[GridButton]
 
 func _ready() -> void:
     game_manager.on_game_data_changed.connect(_game_data_changed)
     game_manager.on_game_won.connect(draw_win_line);
-    for grid_btn in grid_buttons:
+
+    for child in grid_btns_parent.get_children():
+        _grid_buttons.append(child as GridButton);
+
+
+    for grid_btn in _grid_buttons:
         grid_btn.pressed.connect(func(): _on_grid_btn_pressed(grid_btn))
     
     exit_btn.pressed.connect(func(): SceneManager.load_scene(SceneManager.Scenes.MainMenu));
@@ -23,7 +29,7 @@ func _game_data_changed(coords: Vector2i):
     # 1-> x
     # 2 -> o
     # 0 -> None
-    var subject_btn = _find_grid_btns_same_coords(coords);
+    var subject_btn = find_grid_btns_same_coords(coords);
     
     if subject_btn == null:
         print("subject is null")
@@ -36,8 +42,9 @@ func _game_data_changed(coords: Vector2i):
     else:
         subject_btn.set_o_color().set_o_texture();
         
-func _find_grid_btns_same_coords(coords: Vector2i) -> GridButton:
-    for btn in grid_buttons:
+
+func find_grid_btns_same_coords(coords: Vector2i) -> GridButton:
+    for btn in _grid_buttons:
         if btn.coords == coords:
             return btn;
             
@@ -47,10 +54,10 @@ func draw_win_line(_turn: int) -> void:
     var win_pattern = game_manager.get_win_pattern();
     var start_pos = win_pattern[0];
     var end_pos = win_pattern[len(win_pattern) -1];
-    var btn_size = grid_buttons[0].size;
+    var btn_size = _grid_buttons[0].size;
 
-    var start_btn := _find_grid_btns_same_coords(start_pos);
-    var end_btn := _find_grid_btns_same_coords(end_pos);
+    var start_btn := find_grid_btns_same_coords(start_pos);
+    var end_btn := find_grid_btns_same_coords(end_pos);
 
     var start_local = start_btn.get_global_transform_with_canvas().origin + btn_size / 2;
     var end_local = end_btn.get_global_transform_with_canvas().origin + btn_size / 2;
--- scripts/nani_mode/nani_mode_display.gd
+++ scripts/nani_mode/nani_mode_display.gd
@@ -1,25 +1,17 @@
 extends MarginContainer
 
 @export var game_manager: GameManager;
-@export var grid_buttons: Array[GridButton]
+@export var tictactoe_visual: TicTacToeDisplay;
 
 
 func _ready() -> void:
     game_manager.on_data_will_remove_this_move.connect(_data_remove_this_move)
 
 func _data_remove_this_move(coords: Vector2i):
-    var subject_btn := _find_grid_btns_same_coords(coords);
+    var subject_btn := tictactoe_visual.find_grid_btns_same_coords(coords);
 
     if subject_btn == null:
         return;
 
     subject_btn.set_preremove();
-
-func _find_grid_btns_same_coords(coords: Vector2i) -> GridButton:
-    for btn in grid_buttons:
-        if btn.coords == coords:
-            return btn;
-
-    return null;
-
 
